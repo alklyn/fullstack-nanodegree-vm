3c0bd7421b4bfe7bab76d5ec111464d019733a35
alpheus <alklyn@gmail.com>
2016-04-09 14:44:53 +0200

    Define tables.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,23 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+create table tournament(
+    id serial primary key,
+    name text,
+    start_date date,
+    end_date date
+)
 
+create table players(
+    id serial primary key,
+    name text,
+    surname text,
+)
+
+create table matches(
+    tournament_id integer,
+    player1_id integer,
+    player2_id integer,
+    player1_score integer,
+    player2_score integer
+)

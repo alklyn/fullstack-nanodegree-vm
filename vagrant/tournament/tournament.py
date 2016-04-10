05c1623dc5ba0cb84a0c3405dab32b5b311786f9
alpheus <alklyn@gmail.com>
2016-04-10 22:50:38 +0200

    Update db schema to concentrate on one tournament at a time.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,22 +13,39 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    query = "delete from matches;"
+    cur.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    cur = conn.cursor()
+    query = "delete from players;"
+    cur.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    cur = conn.cursor()
+    query = "select count(*) from players;"#Change to support multiple tournaments
+    cur.execute(query)
+    conn.close()
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +73,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +90,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,26 +5,26 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+-- Use the command \i tournament.sql to import the whole file into psql at once.
 
+/*
 create table tournaments(
     id serial primary key,
     name text,
     start_date date,
     end_date date
 );
+*/
 
 create table players(
     id serial primary key,
-    tournament_id integer references tournaments(id),
-    name text,
-    surname text,
+    name text
 );
 
 create table matches(
-    tournament_id integer references tournaments(id),
     player1_id integer references players(id),
     player2_id integer references players(id),
     player1_score integer,
     player2_score integer,
-    primary key (tournament_id, player1_id, player2_id)
+    primary key (player1_id, player2_id)
 );

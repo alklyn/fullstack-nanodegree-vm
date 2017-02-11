819758f5e81db4ad6aaf501ee7f42e3e91bf6b9b
Alpheus Masanga <alklyn@gmail.com>
2017-02-11 23:14:20 +0200

    Fill in the databases.

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -11,26 +11,37 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-    __tableame__ = 'restaurant'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'restaurant'
+    restaurant_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 
 class Menu(Base):
-    __tableame__ = 'menu_item'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'menu_item' # Table information
+
+    # mapper
+    menu_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.restaurant_id'))
     restaurant = relationship(Restaurant)
 
 
 class Employee(Base):
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'employee'
+    employee_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+
+class Address(Base):
+    __tablename__ = 'address'
+    address_id = Column(Integer, primary_key=True)
+    street = Column(String(80), nullable=False)
+    zip_code = Column(String(5), nullable=False)
+    employee_id = Column(Integer, ForeignKey('employee.employee_id'))
+    employee = relationship(Employee)
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')

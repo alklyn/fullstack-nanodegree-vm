7bcc51e944853f7187ccc4e8fde313bad8172613
Alpheus Masanga <alklyn@gmail.com>
2017-02-13 22:31:37 +0200

    Create the sqlite db.

--- vagrant/puppies/database_setup.py
+++ vagrant/puppies/database_setup.py
@@ -2,7 +2,8 @@
 Setup database
 """
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Boolean
+from sqlalchemy import Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -12,17 +13,18 @@ Base = declarative_base()
 
 class Shelter(Base):
     __tablename__ = 'shelter'
+    shelter_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     address = Column(String(250), nullable=False)
     city = Column(String(250), nullable=False)
     state = Column(String(250), nullable=False)
     zip_code = Column(String(250), nullable=False)
     website = Column(String(250), nullable=False)
-    shelter_id = Column(Integer, primary_key=True)
 
 
 class Puppy(Base):
     __tablename__ = 'puppy' # Table information
+    puppy_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     date_of_birth = Column(Date(), nullable=False)
     gender = Column(Boolean(), nullable=False)

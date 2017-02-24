72686e93572c8a6e12b7e600a95efd37343c5311
Alpheus Masanga <alklyn@gmail.com>
2017-02-24 15:12:17 +0200

    Add new folder for the examples on the catalog app.

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -28,6 +28,20 @@ class MenuItem(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    @property
+    def serialize(self):
+        """
+        Return object data in easily serializeable format.
+        """
+        data = {
+            "name": self.name,
+            "description": self.description,
+            "id": self.id,
+            "price": self.price,
+            "course": self.course
+        }
+        return data
+
 
 class Employee(Base):
     __tablename__ = 'employee'
